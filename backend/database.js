0708c2127a55f511c146960a82d41f9c486e2711
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2024-09-01 11:36:14 +0530

    backend done

--- backend/database.js
+++ backend/database.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const DbUrl = process.env.DATABASE_URL;
 
 mongoose.connect(DbUrl).then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB...");
 }).catch(err => {
-    console.error("Failed to connect to MongoDB", err);
+    console.error("Failed to connect to MongoDB...", err);
 });
 
 const userSchema = mongoose.Schema({
@@ -27,9 +27,22 @@ const userSchema = mongoose.Schema({
         required: true,
     }
 })
-
 const User = mongoose.model("User", userSchema);
 
+const accountSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model("Account", accountSchema);
+
 module.exports = ({
-    User
+    User, Account
 })
--- backend/zodValidation.js
+++ backend/zodValidation.js
@@ -1,12 +1,18 @@
 const zod = require("zod");
 
-const userValidation = zod.object({
+const signupValidation = zod.object({
     email: zod.string().email("Invalid email address"),
     firstName: zod.string().min(1, "First name is required"),
     lastName: zod.string().min(1, "Last name is required"),
     password: zod.string().min(6, "Password must be at least 6 characters long")
 });
 
+const signinValidation = zod.object({
+    email: zod.string().email("Invalid email address"),
+    password: zod.string().min(6, "Password must be at least 6 characters long")
+});
+
 module.exports = {
-    userValidation
+    signupValidation,
+    signinValidation
 };
