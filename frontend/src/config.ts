dc1a2e97ec1edaf0d3479dc903859a3993a61cd1
HanuCh@udhary <137854084+hanuchaudhary@users.noreply.github.com>
2024-09-24 00:57:42 +0530

    changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL =  "https://_backend.kushchaudharyog.workers.dev";
+export const SERVER_URL =  "https://repay-backend.kushchaudharyog.workers.dev";
 // export const SERVER_URL =  "http://127.0.0.1:8787";
